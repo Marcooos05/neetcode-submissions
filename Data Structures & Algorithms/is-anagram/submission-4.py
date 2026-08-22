d499b20ecb82bc951e41730b63cfaa6841d44f53
Marcooos05 <108853663+Marcooos05@users.noreply.github.com>
2026-08-22 21:24:51 +0800

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #optimal memory allocation
+        #Runtime 45ms|Beats 100.00%
+        #Memory 9.3 MB|Beats 5.10%
+        return sorted(s) == sorted(t)
